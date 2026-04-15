d0c8de07c1b7b9f39df92917500351b65e971df3
Lsilinda <eric.silinda@gmail.com>
2026-04-15 11:56:49 +0200

    Initial scaffold

---
+++ data/README.md
@@ -0,0 +1,10 @@
+﻿# Data directory
+
+This directory is gitignored. It holds references to the 70 synthetic source
+files used for the migration project.
+
+Document here:
+- Where the files live (Azure Storage path, local path)
+- File naming convention
+- Schemas / column descriptions
+- Any data quality notes
---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+﻿terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.95"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "snowflake" {}
+provider "azurerm" {
+  features {}
+}
